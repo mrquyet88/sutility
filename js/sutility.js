c7183b377bf599872e3d55ca3a050c8c4e0e2b95
MrQuyet <mrquyet88@gmail.com>
2017-05-29 14:32:58 +0700

    Update sResize

--- js/sutility.js
+++ js/sutility.js
@@ -5,7 +5,7 @@
 	license : imgspec.com/sutilitiy
 */
 $.fn.extend({
-	/*Set equal height or width of column*/
+	/* Set equal height or width of column*/
 	sColumn: function(options){
 		var col = 0;
 		var max_h = 0;
@@ -32,64 +32,71 @@ $.fn.extend({
 			})
 		}
 		
-		var timer = false;
 		var selector = $(this.selector);
-		init();
-		$(window).resize(function() {
-			init();
+		$(window).sResize(function(){
+			/*Cacl colum, width, marginright*/
+			var ww = $(window).width();
+			Object.keys(paras.breakPoint).forEach(function(key) {
+				if( ww >= key){
+					col = Math.abs(paras.breakPoint[key].column);
+					max_w = Math.abs(parseInt(paras.breakPoint[key].width));
+					if(col > 1){
+						marginRight = Math.abs((100 - col*max_w)/(col - 1));
+					}
+				}
+			})
+			/*Calc height*/
+			selector.each(function(){
+				var from = 0;
+				var cur_selector = $(this).children();
+				cur_selector.removeAttr('style');
+				if(col > 1){
+					cur_selector.css({'float': 'left', 'width': max_w + '%'});
+					cur_selector.each(function($i){
+						if( $i % col == 0){
+							from = $i;
+							max_h = 0;
+						}
+						if($(this).outerHeight() > max_h){
+							max_h = $(this).outerHeight();
+						}
+						/*Add style for element*/
+						cur_selector.each(function($i2){
+							if($i2 >= from && $i2 <= $i){
+								$css = {'height' : max_h}
+								if(paras.setWidth && max_w > 0){
+									$css = $.extend($css, {
+										'width': max_w + '%',
+										'float': 'left'
+									});
+									if(!($i2 == $i || $i2 == cur_selector.length)){
+										$css = $.extend($css, {
+											'margin-right': marginRight + '%'
+										});
+									}
+								}
+								$(this).css($css);
+							}
+						})
+					})
+				}
+			})
 		});
-		function init(){
+	},
+	/* Smart resize windown*/
+	sResize: function(callback){
+		// first loaded run
+		$(window).load(function(){
+			callback.call(this);
+		});
+
+		//resize run
+		var timer = false;
+		$(window).resize(function() {
 			if (timer) clearTimeout(timer);
 			timer = setTimeout(function(){
-				/*Cacl colum, width, marginright*/
-				var ww = $(window).width();
-				Object.keys(paras.breakPoint).forEach(function(key) {
-					if( ww >= key){
-						col = Math.abs(paras.breakPoint[key].column);
-						max_w = Math.abs(parseInt(paras.breakPoint[key].width));
-						if(col > 1){
-							marginRight = Math.abs((100 - col*max_w)/(col - 1));
-						}
-					}
-				})
-				/*Calc height*/
-				selector.each(function(){
-					var from = 0;
-					var cur_selector = $(this).children();
-					cur_selector.removeAttr('style');
-					if(col > 1){
-						cur_selector.css({'float': 'left', 'width': max_w + '%'});
-						cur_selector.each(function($i){
-							if( $i % col == 0){
-								from = $i;
-								max_h = 0;
-							}
-							if($(this).outerHeight() > max_h){
-								max_h = $(this).outerHeight();
-							}
-							/*Add style for element*/
-							cur_selector.each(function($i2){
-								if($i2 >= from && $i2 <= $i){
-									$css = {'height' : max_h}
-									if(paras.setWidth && max_w > 0){
-										$css = $.extend($css, {
-											'width': max_w + '%',
-											'float': 'left'
-										});
-										if(!($i2 == $i || $i2 == cur_selector.length)){
-											$css = $.extend($css, {
-												'margin-right': marginRight + '%'
-											});
-										}
-									}
-									$(this).css($css);
-
-								}
-							})
-						})
-					}
-				})
+				callback.call(this);
 			}, 300);
-		}
+		});
 	}
 })
